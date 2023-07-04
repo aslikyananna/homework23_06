cd69fe3eeece73bbe2d6b87e1968adc5d8a65704
aslikyananna <135046932+aslikyananna@users.noreply.github.com>
2023-07-04 18:22:40 +0400

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,9 +1,8 @@
 def is_sortedde(x):
     for i in range(len(x)):
-        if x[i] < x[i+1] or x[i] == x[i+1]:
+        if x[i] < x[i+1]:
             return True
         else:
             return False
-sort = is_sortedde([8,8,8,9])
+sort = is_sortedde([8,8,4,9,7])
 print(sort)
-
